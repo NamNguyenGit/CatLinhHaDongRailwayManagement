02f21cb9a827b78cfdff4c3baacd04d43fa99b41
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-01-02 18:49:49 +0700

    add User entities

---
+++ Documents/NetBeansProjects/CLHDrailways/src/Entities/User.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author acer
+ */
+public class User {
+    private int Id ;
+    private String Name;
+    private String Dob;
+    private String Gender;
+    private String Phone;
+    private String Role;
+    private String Username;
+    private String Password;
+
+    public User() {
+    }
+
+    public User(int Id, String Name, String Dob, String Gender, String Phone, String Role, String Username, String Password) {
+        this.Id = Id;
+        this.Name = Name;
+        this.Dob = Dob;
+        this.Gender = Gender;
+        this.Phone = Phone;
+        this.Role = Role;
+        this.Username = Username;
+        this.Password = Password;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDob() {
+        return Dob;
+    }
+
+    public void setDob(String Dob) {
+        this.Dob = Dob;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+    
+}
