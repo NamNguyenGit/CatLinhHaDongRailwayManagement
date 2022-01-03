e8dd34a9545aa18c350aeb4048f2326ae23abc76
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-01-03 14:15:04 +0700

    Done Login User Frames

--- Documents/NetBeansProjects/CLHDrailways/src/AllFrames/ClientLoginFrame.java
+++ Documents/NetBeansProjects/CLHDrailways/src/AllFrames/ClientLoginFrame.java
@@ -11,7 +11,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -22,16 +21,16 @@ import javax.swing.UnsupportedLookAndFeelException;
  *
  * @author acer
  */
-public class AdminLoginFrame extends javax.swing.JFrame {
+public class ClientLoginFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form AdminLoginFrame
+     * Creates new form ClientLoginFrame
      */
-    public AdminLoginFrame() {
+    public ClientLoginFrame() {
          try {
             UIManager.setLookAndFeel(new WindowsLookAndFeel());
         } catch (UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(AdminLoginFrame.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ClientLoginFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
         initComponents();
         setLocationRelativeTo(null);
@@ -47,76 +46,58 @@ public class AdminLoginFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        password = new javax.swing.JPasswordField();
         username = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        password = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(750, 519));
-        setResizable(false);
+        setPreferredSize(new java.awt.Dimension(852, 500));
 
         jPanel1.setLayout(null);
-
-        password.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                passwordActionPerformed(evt);
-            }
-        });
-        jPanel1.add(password);
-        password.setBounds(280, 280, 190, 22);
         jPanel1.add(username);
-        username.setBounds(280, 190, 190, 22);
+        username.setBounds(350, 200, 220, 22);
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icons8-back-arrow-48.png"))); // NOI18N
-        jLabel6.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel6MouseClicked(evt);
-            }
-        });
-        jPanel1.add(jLabel6);
-        jLabel6.setBounds(0, 0, 50, 50);
+        jLabel3.setFont(new java.awt.Font("Segoe UI Black", 3, 18)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("PASSWORD");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(170, 270, 130, 40);
+        jPanel1.add(password);
+        password.setBounds(350, 280, 220, 22);
 
-        jButton1.setBackground(new java.awt.Color(0, 102, 255));
-        jButton1.setFont(new java.awt.Font("Segoe UI Black", 3, 36)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setBackground(new java.awt.Color(255, 153, 102));
+        jButton1.setFont(new java.awt.Font("Segoe UI Black", 3, 18)); // NOI18N
         jButton1.setText("LET'S GO");
-        jButton1.setBorder(null);
-        jButton1.setBorderPainted(false);
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(270, 400, 210, 50);
+        jButton1.setBounds(320, 380, 140, 40);
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI Black", 3, 18)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("PASSWORD");
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icons8-back-arrow-48.png"))); // NOI18N
+        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel4MouseClicked(evt);
+            }
+        });
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(110, 270, 130, 40);
+        jLabel4.setBounds(0, 0, 50, 60);
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI Black", 3, 18)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("USERNAME");
-        jPanel1.add(jLabel3);
-        jLabel3.setBounds(110, 180, 130, 40);
-
-        jLabel2.setFont(new java.awt.Font("Segoe UI Black", 3, 48)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Segoe UI Black", 3, 18)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("LOGIN");
+        jLabel2.setText("USERNAME");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(250, -20, 270, 140);
+        jLabel2.setBounds(170, 190, 130, 40);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/admin.jpg"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/1.jpg"))); // NOI18N
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(0, 0, 750, 520);
+        jLabel1.setBounds(0, 0, 850, 480);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -132,6 +113,13 @@ public class AdminLoginFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseClicked
+        // TODO add your handling code here:
+        WelcomeFrame WF = new WelcomeFrame();
+        WF.show();
+        dispose();
+    }//GEN-LAST:event_jLabel4MouseClicked
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         Connection conn;
         conn = Database_Connect.getConnection();
@@ -141,34 +129,22 @@ public class AdminLoginFrame extends javax.swing.JFrame {
         
         try {
             Statement stm = conn.createStatement();
-            String sql = "select * from user where role = 1 and username ='" + user + "' and password = '" + pass + "' ";
+            String sql = "select * from user where role = 2 and username ='" + user + "' and password = '" + pass + "' ";
             ResultSet rs = stm.executeQuery(sql);
             
             if (rs.next()) {
-                AdminHomeFrame ahf = new AdminHomeFrame();
-                ahf.show();
+               BuyTicketFrame BTF = new BuyTicketFrame();
+                BTF.show();
                 dispose();
             } else {
-                JOptionPane.showMessageDialog(this, "Username or Password is wrong or You are not Admin Yet ");
+                JOptionPane.showMessageDialog(this, "Username or Password is wrong.");
                 username.setText(null);
                 password.setText(null);
             }
         } catch (SQLException ex) {
             Logger.getLogger(AdminLoginFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void passwordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_passwordActionPerformed
-
-    private void jLabel6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel6MouseClicked
-        // TODO add your handling code here:
-        WelcomeFrame WF = new WelcomeFrame();
-        WF.show();
-        dispose();
-    }//GEN-LAST:event_jLabel6MouseClicked
 
     /**
      * @param args the command line arguments
@@ -187,20 +163,20 @@ public class AdminLoginFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdminLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdminLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdminLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdminLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientLoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AdminLoginFrame().setVisible(true);
+                new ClientLoginFrame().setVisible(true);
             }
         });
     }
@@ -211,7 +187,6 @@ public class AdminLoginFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField password;
     private javax.swing.JTextField username;
