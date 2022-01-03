c18a2ef33ad7d48a886b4e407f2fb70fff62074c
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-01-03 09:23:50 +0700

    Schedule Implements

---
+++ Documents/NetBeansProjects/CLHDrailways/src/DAO/SheduleDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.List;
+import Entities.Schedule;
+/**
+ *
+ * @author acer
+ */
+public interface SheduleDAO {
+    public List<Schedule> getlistSchedule();
+    public boolean insertSchedule(Schedule S1);
+    public boolean deleteSchedule(int IdSchedule);
+    public boolean updateSchedule(Schedule d1);
+    public List<Schedule> getDrinksbyTrainID(int TrianID);
+}
